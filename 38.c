45c06ad3a6545a5bb03d47268ca6042a57dafec8
yemihuang <306837679@qq.com>
2016-08-30 21:07:48 +0800

    program 38

---
+++ 38.c
@@ -0,0 +1,30 @@
+#include "head.h"
+#define N 3
+
+void fun(int a[N][N])
+{
+	int sum = 0;
+	
+	for (int i = 0; i < N; ++i)
+	{
+		sum += a[i][i];
+	}
+	printf("%d\n", sum);
+}
+
+int main()
+{
+	//OPEN_URL(__FILE__);
+	int a[N][N];
+
+	for (int i = 0; i < N; ++i)
+	{
+		for (int j = 0; j < N; ++j)
+		{
+			scanf("%d", &a[i][j]);
+		}
+	}
+	fun(a);
+	
+	return 0;
+}
